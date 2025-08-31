f33890eb28276dee371132738691e40ba66f5a62
dibhell <dib.hell@gmail.com>
2025-08-31 20:11:40 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path"; // ważne
 
 export default defineConfig({
   plugins: [react()],
-  base: "/teksciorz/",
+  base: "/teksciorz/", // albo nazwa Twojego repo
+  resolve: {
+    alias: { "@": resolve(__dirname, "src") }, // <-- alias @ -> src
+  },
 });
