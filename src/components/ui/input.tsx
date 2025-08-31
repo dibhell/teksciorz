497123ea9a76c88f62f2f2af5b2d7c960a55d23b
dibhell <dib.hell@gmail.com>
2025-08-31 22:24:22 +0200

    Update input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 
 export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
-  ({ className, ...props }, ref) => (
+  ({ className = "", ...props }, ref) => (
     <input
       ref={ref}
-      className={`w-full rounded-2xl border px-3 py-2 dark:bg-gray-900 dark:text-white ${className}`}
+      className={`w-full rounded-2xl border px-3 py-2 bg-white dark:bg-slate-900 dark:text-white ${className}`}
       {...props}
     />
   )
