e7ef8a4a5794918003558696614a889541ba0184
dibhell <dib.hell@gmail.com>
2025-08-31 22:24:08 +0200

    Update textarea.tsx

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 
 export const Textarea = React.forwardRef<HTMLTextAreaElement, React.TextareaHTMLAttributes<HTMLTextAreaElement>>(
-  ({ className, ...props }, ref) => (
+  ({ className = "", ...props }, ref) => (
     <textarea
       ref={ref}
-      className={`w-full rounded-2xl border p-2 dark:bg-gray-900 dark:text-white ${className}`}
+      className={`w-full rounded-2xl border p-2 bg-white dark:bg-slate-900 dark:text-white ${className}`}
       {...props}
     />
   )
