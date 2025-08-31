3c72fb4946fb0a445e5a455ea1feb8cf3ba78f0e
dibhell <dib.hell@gmail.com>
2025-08-31 14:35:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// base: './' działa na GitHub Pages bez konieczności wpisywania nazwy repozytorium
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/teksciorz/",
 });
