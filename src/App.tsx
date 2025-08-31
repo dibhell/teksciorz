6dd976aa93aa4aef23cb103473c879b0ead877fc
dibhell <dib.hell@gmail.com>
2025-08-31 14:33:13 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
+      <h1>Rytmika Tekstu</h1>
+      <p>Build test – ten komponent możesz zastąpić swoim z Canvas.</p>
+    </div>
+  );
+}
